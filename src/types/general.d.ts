12c6baa1ddb76feac5a9ddae582acece8a3a4a81
Bryan Kizer <brykizer@gmail.com>
2020-01-23 12:57:44 -0500

    chore: ts setup

---
+++ src/types/general.d.ts
@@ -0,0 +1,29 @@
+type nil = null | undefined;
+
+type Nilable<T> = T | nil;
+
+type Nullable<T> = T | null;
+
+type Undefinable<T> = T | undefined;
+
+type Primitive = boolean | string | number;
+
+type Truthy = boolean | string | number;
+
+type Falsey = boolean | string | number | null | undefined;
+
+type BoolLike = Truthy | Falsey;
+
+type AnyKey = string | number | symbol;
+
+type AnyFunction = (...args: any) => any;
+
+interface AnyClass {
+  prototype: any;
+  new (...args: any): any;
+}
+
+interface AnyObject {
+  [key: string]: any;
+  [key: number]: any;
+}
